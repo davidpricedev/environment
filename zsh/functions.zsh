dc65d8c35c1777efe642fae6b17af7f9272f45ed
davidpricedev <davidpricedev@gmail.com>
2026-07-11 09:15:46 -0500

    cleanup

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -224,13 +224,6 @@ gitw-add() {
   cd -
 }
 
-###--- Wrap git diff in a function so we get some auto-complete happening ---###
-fdiff() {
-  local f1="$1"
-  local f2="$2"
-  git diff "$f1" "$f2"
-}
-
 ###--- Python Cleanup ---###
 pyclean () {
   find . -type f -name '*.py[co]' -delete -o -type d -name __pycache__ -delete
@@ -251,6 +244,7 @@ function chpwd {
     fi
 }
 
+###--- List listening Ports ---###
 function list-ports {
   local isLinux
   isLinux=false
